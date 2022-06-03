bf35dbfaba98a2c0f841d7f68f05f32db882efa2
JUAN FALCON <JUANCANEN_10_@HOTMAIL.COM>
2022-06-03 09:59:01 -0500

    Added initial config project with ng-zorro and lazy loading

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin.component';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+@NgModule({
+  declarations: [
+    AdminComponent
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    NzLayoutModule,
+    NzIconModule,
+    NzBreadCrumbModule,
+    NzMenuModule
+  ]
+})
+export class AdminModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('src/app/login/login.module').then((m) => m.LoginModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('src/app/admin/admin.module').then((m) => m.AdminModule)
+  },
+  {
+    path: '', pathMatch: 'full', redirectTo: 'login'
+  },
+  {
+    path: '**', pathMatch: 'full', redirectTo: 'login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
